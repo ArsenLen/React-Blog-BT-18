eda8918cdca97322f92bcce3f7a33717c9ce035a
ArsenLen <laurie10@mail.ru>
2023-03-04 20:09:11 +0600

    decomposition, props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello/Hello'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Приветствие</h1>
+      <Hello name="Arsen" age="25" />
+      <Hello name="Kenan" />
+      <Hello name="Kairat" />
     </div>
   );
 }
 
 export default App;
+// Feature Slices 
+// БЭМ - методология
+// Компонент - функция, которая возвращает JSX(HTML+JS) разметку
+// CTRL+A - выделить все
+// CTRL+K+F - форматировать код
+
+/*
+  ДЗ. Создать Компонент Hobby
+  1. В компоненте в теге h5 должны отображаться хобби, которые вы передаете как пропс
+  Вызвать компонент Hobby под компонентом Hello каждого пользователя
+  2. В компонент Hello так же передавать пропс age и отобразите его в другом
+  теге p внутри компонента hello. Передать age для каждого <Hello />
+*/
